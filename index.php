583441cdc5861b96555723b1490477b13540abbb
ThejasVK <71866416+ThejasVK@users.noreply.github.com>
2022-10-18 15:57:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HTML 5 Boilerplate</title>
     <link rel="stylesheet" href="style.css">
   </head>
@@ -12,6 +8,6 @@
 	<!--<script src="index.js"></script>-->
 	<?php 
 	     echo "Hi there";
-	     ?>
+	?>
   </body>
 </html>
